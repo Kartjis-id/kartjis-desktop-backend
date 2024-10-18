2cf59a256c5d380cab3e961524db73e44890958f
toku404 <phantom26isn@gmail.com>
2024-10-18 19:42:38 +0800

    feat

--- routes/ticket.py
+++ routes/ticket.py
@@ -60,7 +60,7 @@ async def read_data(response: Response):
             INNER JOIN orderdetails AS od ON tv.orderDetailId=od.id
             INNER JOIN tickets AS t ON od.ticketId=t.id
             inner join orders o on od.orderId = o.id
-            WHERE t.eventId = 'a28fa37e-6782-43ed-9353-5ec7d7e967be' and o.status = "SUCCESS"
+            WHERE t.eventId = '25bdff39-fdd0-4a8f-9435-c69bb5370d3a' and o.status = "SUCCESS"
             order by od.NAME
             """
             result_proxy = await conn.execute(text(query))
@@ -96,7 +96,7 @@ async def update_verification2(hash: str):
                 INNER JOIN orderdetails AS od ON tv.orderDetailId = od.id
                 INNER JOIN tickets AS t ON od.ticketId = t.id
                 inner join orders o on od.orderId = o.id
-                WHERE tv.HASH = :hash AND t.eventId = 'a28fa37e-6782-43ed-9353-5ec7d7e967be' and o.status = "SUCCESS"
+                WHERE tv.HASH = :hash AND t.eventId = '25bdff39-fdd0-4a8f-9435-c69bb5370d3a' and o.status = "SUCCESS"
             """
 
             check_result = await conn.execute(text(check_query), {"hash": hash})
@@ -135,7 +135,7 @@ async def update_verification2(hash: str):
                 INNER JOIN orderdetails AS od ON tv.orderDetailId = od.id
                 INNER JOIN tickets AS t ON od.ticketId = t.id
                 SET tv.isScanned = :is_verified, tv.updatedAt = :current_datetime
-                WHERE tv.HASH = :hash AND t.eventId = 'a28fa37e-6782-43ed-9353-5ec7d7e967be'
+                WHERE tv.HASH = :hash AND t.eventId = '25bdff39-fdd0-4a8f-9435-c69bb5370d3a'
             """
 
             current_datetime = datetime.datetime.now()
@@ -174,19 +174,22 @@ async def update_verification2(hash: str):
             "error": str(e),
             "ticket": {}  # Empty ticket data in case of error
         }
-
 @ticket.put('/api/tickets/{hash}/verification')
-async def update_verification(hash: str, response: Response):
+async def update_verification(hash: str, request: Request, response: Response):
     try:
+        # Ambil data dari body request
+        body = await request.json()
+        isVerify = body.get('isVerify')  # Mengambil nilai isVerify dari body
+
         async with engine.begin() as conn:
-            # Check if ticket is already verified for verification, or not verified for unverification
+            # Check if ticket is already verified or not
             check_query = """
                 SELECT tv.isScanned, od.name, od.email, od.ticketId, tv.hash, t.name, od.orderId, tv.updatedAt
                 FROM ticketverification AS tv
                 INNER JOIN orderdetails AS od ON tv.orderDetailId = od.id
                 INNER JOIN tickets AS t ON od.ticketId = t.id
-                inner join orders o on od.orderId = o.id
-                WHERE tv.HASH = :hash AND t.eventId = 'a28fa37e-6782-43ed-9353-5ec7d7e967be' and o.status = "SUCCESS"
+                INNER JOIN orders o ON od.orderId = o.id
+                WHERE tv.HASH = :hash AND t.eventId = '25bdff39-fdd0-4a8f-9435-c69bb5370d3a' AND o.status = "SUCCESS"
             """
 
             check_result = await conn.execute(text(check_query), {"hash": hash})
@@ -197,8 +200,32 @@ async def update_verification(hash: str, response: Response):
                     status_code=404, detail="Ticket not found"
                 )
 
-            verification_status = row[0]  # Mengambil nilai pertama dari baris hasil query
+            verification_status = row[0]  # Mengambil nilai isScanned dari hasil query
             ticket_info = row[1:]
+
+            # Jika isVerify True dan tiket sudah diverifikasi, kembalikan error 400
+            if isVerify and verification_status:
+                response.status_code = 400  # Set status code 400
+                return {
+                    "success": False,
+                    "error": "Ticket already verified",
+                    "ticket": {
+                        "customerName": ticket_info[0],
+                        "customerEmail": ticket_info[1],
+                        "invoiceId": ticket_info[2],
+                        "verifiedAt": row[7],  # Menggunakan updatedAt dari data sebelumnya
+                        "ticketType": ticket_info[4],
+                        "isVerified": int(verification_status),
+                        "hash": ticket_info[3],
+                        "ticketNum": 1  # Asumsikan ticketNum tetap sama, bisa disesuaikan sesuai logika
+                    }
+                }
+
+            # Jika isVerify False dan tiket sudah diverifikasi, ubah isScanned menjadi False
+            if not isVerify and verification_status:
+                is_verified = 0  # Membatalkan verifikasi
+            else:
+                is_verified = 1 if isVerify else 0  # Mengatur nilai isScanned sesuai isVerify
 
             get_num_query = """
                 SELECT tv.hash FROM tickets t
@@ -216,41 +243,32 @@ async def update_verification(hash: str, response: Response):
                     ticket_num = i+1
                     break
 
-            is_verified = 1 if int(verification_status) == 0 else 0
-
-
-            # Update verification status based on isVerify parameter
+            # Update verification status
             query_update = """
                 UPDATE ticketverification AS tv
                 INNER JOIN orderdetails AS od ON tv.orderDetailId = od.id
                 INNER JOIN tickets AS t ON od.ticketId = t.id
                 SET tv.isScanned = :is_verified, tv.updatedAt = :current_datetime
-                WHERE tv.HASH = :hash AND t.eventId = 'a28fa37e-6782-43ed-9353-5ec7d7e967be'
+                WHERE tv.HASH = :hash AND t.eventId = '25bdff39-fdd0-4a8f-9435-c69bb5370d3a'
             """
 
             current_datetime = datetime.datetime.now()
 
             result = await conn.execute(text(query_update), {"hash": hash, "is_verified": is_verified, "current_datetime": current_datetime})
-            if result.rowcount == 0:
-                raise HTTPException(
-                    status_code=404, data="Ticket not found or already scanned"
-                )
-
-            # await send_notification({"message": "Ticket verification updated.", "event": "verification" if is_verify else "unverification"})
 
             # Construct data response including ticket information
             data = {
                 "success": True,
-                "message": "Success verify ticket" if verification_status else "Success unverify ticket",
+                "message": "Success verify ticket" if isVerify else "Success unverify ticket",
                 "ticket": {
                     "customerName": ticket_info[0],
                     "customerEmail": ticket_info[1],
                     "invoiceId": ticket_info[2],
-                    "verifiedAt" : current_datetime,
+                    "verifiedAt": current_datetime if isVerify else None,
                     "ticketType": ticket_info[4],
-                    "isVerified" : int(verification_status),
-                    "hash" :  ticket_info[3],
-                    "ticketNum" : ticket_num
+                    "isVerified": is_verified,
+                    "hash": ticket_info[3],
+                    "ticketNum": ticket_num
                 }
             }
 
@@ -265,6 +283,7 @@ async def update_verification(hash: str, response: Response):
             "error": str(e),
             "ticket": {}  # Empty ticket data in case of error
         }
+    
 
 @ticket.get('/api/outlier')
 async def read_data(response: Response):
@@ -279,7 +298,7 @@ async def read_data(response: Response):
                 FROM orders o
                 JOIN orderdetails od ON od.orderId = o.id
                 JOIN events e ON e.id = o.eventId
-                WHERE o.eventId = "a28fa37e-6782-43ed-9353-5ec7d7e967be"
+                WHERE o.eventId = "25bdff39-fdd0-4a8f-9435-c69bb5370d3a"
                 GROUP BY od.orderId
                 HAVING COUNT(DISTINCT od.email) > 1
             );
